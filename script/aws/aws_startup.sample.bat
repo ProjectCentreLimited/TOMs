126b065587c80d347ef3f81d718264a11f732d06
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2022-09-26 18:36:03 +0200

    fix aws_startup.bat

--- script/aws/aws_startup.sample.bat
+++ script/aws/aws_startup.sample.bat
@@ -5,7 +5,7 @@ net use S: \\samba.private-projectcenter.com\appstream 2022u6 /user:ubuntu
 set DEPLOY_ROOT_DIR=\\samba.private-projectcenter.com\appstream\live
 set DEPLOY_CONFIG_FILE=%DEPLOY_ROOT_DIR%\aws_deploy.conf
 
-"C:\Program Files\QGIS 3.22.10\bin\python-qgis-ltr.bat" %DEPLOY_ROOT_DIR%\qgis_plugin\script\aws\aws_deploy.py
+call "C:\Program Files\QGIS 3.22.10\bin\python-qgis-ltr.bat" %DEPLOY_ROOT_DIR%\qgis_plugin\script\aws\aws_deploy.py
 
-timeout /T 10
+timeout /T 10 /NOBREAK
 net use S: /delete
