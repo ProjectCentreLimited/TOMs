b69a8cfe4a6358a90ba4c805152736667137a12a
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2022-09-27 17:52:28 +0200

    DEPLOY.md: update doc and add sample scripts

--- script/aws/aws_startup.sample.bat
+++ script/aws/aws_startup.sample.bat
@@ -1,5 +1,7 @@
 @echo off
+del /Q c:\script\startup_done
 net use S: /delete
+
 net use S: \\samba.private-projectcenter.com\appstream 2022u6 /user:ubuntu
 
 set DEPLOY_ROOT_DIR=\\samba.private-projectcenter.com\appstream\live
@@ -7,5 +9,7 @@ set DEPLOY_CONFIG_FILE=%DEPLOY_ROOT_DIR%\aws_deploy.conf
 
 call "C:\Program Files\QGIS 3.22.10\bin\python-qgis-ltr.bat" %DEPLOY_ROOT_DIR%\qgis_plugin\script\aws\aws_deploy.py
 
-timeout /T 10 /NOBREAK
+timeout /T 5 /NOBREAK
 net use S: /delete
+
+echo "true" > c:\script\startup_done
